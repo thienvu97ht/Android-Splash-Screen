d70331fbba7d6c5c2e600d469ee66dafd0123382
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-03-10 13:40:48 +0700

    Final

--- app/src/main/java/com/example/splashscreen/IntroductoryActivity.java
+++ app/src/main/java/com/example/splashscreen/IntroductoryActivity.java
@@ -1,6 +1,11 @@
 package com.example.splashscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -13,6 +18,10 @@ public class IntroductoryActivity extends AppCompatActivity {
     ImageView logo, appName, splashImg;
     LottieAnimationView lottieAnimationView;
 
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +33,42 @@ public class IntroductoryActivity extends AppCompatActivity {
         splashImg = findViewById(R.id.img);
         lottieAnimationView = findViewById(R.id.lottie);
 
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
         splashImg.animate().translationY(-3000).setDuration(1000).setStartDelay(4000);
-        logo.animate().translationY(1700).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
         appName.animate().translationY(1700).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(1700).setDuration(1000).setStartDelay(4000);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case  0:
+                    OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                    return tab1;
+                case  1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+                case  2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
